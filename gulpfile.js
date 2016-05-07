a5eff48ce86658e18cf541ce51f7f1d53cf52b98
Scott Ludwig <scottlu@gmail.com>
2016-05-07 14:33:54 -0700

    Minor gulpfile improvement.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,10 @@
 var gulp = require('gulp');
 var run = require('gulp-run');
 
-gulp.task('main-build', function() {
-    return run('cd src && tsc').exec();
-});
-
-gulp.task('build', ['main-build'], function() {
-    return run('cp src/navbarPart.css out-build').exec();
+gulp.task('build', function() {
+    return run('cd src && tsc').exec(function() {
+        return run('cp src/navbarPart.css out-build').exec();
+    });
 });
 
 gulp.task('clean', function() {
