209c68a73fe05aa27a9a57733fed48e37860af3b
Scott Ludwig <scottlu@gmail.com>
2016-05-07 21:37:21 -0700

    Change clean to use rsync

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,7 @@ var gulp = require('gulp');
 var shell = require('gulp-shell');
 
 gulp.task('clean', shell.task([
-    'rm -rf out-build',
-    'cp -R $(cd "$(cd "src/vs" && echo $(pwd -P))"/../../out && echo $PWD) out-build'
+    'rsync -a --delete $(cd "$(cd "src/vs" && echo $(pwd -P))"/../../out && echo $PWD)/ out-build'
 ]));
 
 gulp.task('build', shell.task([
