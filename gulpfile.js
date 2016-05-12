1e7ea0daa02b782a94f5c70cc6db653e649d2282
Scott Ludwig <scottlu@gmail.com>
2016-05-11 18:06:54 -0700

    Make build and watch tasks depend on a pre-build task.

--- gulpfile.js
+++ gulpfile.js
@@ -5,15 +5,16 @@ gulp.task('clean', shell.task([
     'rsync -a --delete $(cd "$(cd "src/vs" && echo $(pwd -P))"/../../out && echo $PWD)/ out-build'
 ]));
 
-gulp.task('build', shell.task([
+gulp.task('pre-build', shell.task([
     'cp index.html out-build',
     'mkdir -p out-build/forked && cp src/forked/navbarPart.css out-build/forked',
+]));
+
+gulp.task('build', ['pre-build'], shell.task([
     'cd src && tsc'
 ]));
 
-gulp.task('watch', shell.task([
-    'cp index.html out-build',
-    'mkdir -p out-build/forked && cp src/forked/navbarPart.css out-build/forked',
+gulp.task('watch', ['pre-build'], shell.task([
     'cd src && tsc -w'
 ]));
 
