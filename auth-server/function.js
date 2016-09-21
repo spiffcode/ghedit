99fe1d857cc62102a4206115a7aad606b16c7a77
Darrin Massena <darrin@massena.com>
2016-09-21 15:37:35 -0700

    Read auth-server config from Google Cloud Storage

--- auth-server/function.js
+++ auth-server/function.js
@@ -1,14 +1,7 @@
 var https = require('https');
+var storage = require('@google-cloud/storage')();
 
 exports.auth = function (request, response) {
-	// TODO: move these to a config file and reset the client secrets
-	// https://github.com/organizations/spiffcode/settings/applications/356856
-	var LOCALHOST_CLIENT_ID = '60d6dd04487a8ef4b699';
-	var LOCALHOST_CLIENT_SECRET = '685d547286d7a983a68cd4f9dde8943515a7affd';
-	// https://github.com/organizations/spiffcode/settings/applications/354499
-	var GITHUB_CLIENT_ID = 'bbc4f9370abd2b860a36';
-	var GITHUB_CLIENT_SECRET = '9aea005378d783e692332bf1bd1e6ca696c8a8b2';
-
 	response.setHeader('Access-Control-Allow-Origin', '*');
 	response.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
 
@@ -18,27 +11,39 @@ exports.auth = function (request, response) {
 	}
 
 	console.log('ghauth ' + JSON.stringify(request.headers));
-	var localhost = request.headers.origin ? (request.headers.origin.indexOf('localhost') != -1) : false;
-	console.log('localhost: ' + localhost);
-	var client = 'client_id=' + (localhost ? LOCALHOST_CLIENT_ID : GITHUB_CLIENT_ID) + 
-			'&client_secret=' + (localhost ? LOCALHOST_CLIENT_SECRET : GITHUB_CLIENT_SECRET);
-	var options = {
-		hostname: 'github.com', port: 443, method: 'POST',
-		path: '/login/oauth/access_token?' + client + '&code=' + request.query.code
-	};
-	var req = https.request(options, function(res) {
-		console.log('Status: ' + res.statusCode);
-		res.setEncoding('utf8');
-		res.on('data', function (body) {
-			console.log('Body: ' + body);
-			response.statusCode = res.statusCode;
-			response.statusMessage = res.statusMessage;
-			response.end(body);
+
+	storage.bucket('ghcodex.appspot.com').file('config.json').download(function (err, data) {
+//		console.log('configFile.download: err: ' + JSON.stringify(err) + '\ndata: ' + data);
+		if (err) {
+			console.log('config.json download err: ' + err);
+			response.sendStatus(500);
+			return;
+		}
+
+		var config = JSON.parse(data);
+//		console.log('config: ' + JSON.stringify(config));
+
+		var localhost = request.headers.origin ? (request.headers.origin.indexOf('localhost') != -1) : false;
+		var client = localhost ? config['localhost'] : config['spiffcode.github.io'];
+		var clientParms = 'client_id=' + client.id + '&client_secret=' + client.secret;
+		var options = {
+			hostname: 'github.com', port: 443, method: 'POST',
+			path: '/login/oauth/access_token?' + clientParms + '&code=' + request.query.code
+		};
+		var req = https.request(options, function(res) {
+			console.log('Status: ' + res.statusCode);
+			res.setEncoding('utf8');
+			res.on('data', function (body) {
+				console.log('Body: ' + body);
+				response.statusCode = res.statusCode;
+				response.statusMessage = res.statusMessage;
+				response.end(body);
+			});
 		});
+		req.on('error', function(e) {
+			console.log('problem with request: ' + e.message);
+			response.sendStatus(res.statusCode);
+		});
+		req.end();
 	});
-	req.on('error', function(e) {
-		console.log('problem with request: ' + e.message);
-		response.sendStatus(res.statusCode);
-	});
-	req.end();
 }
